4928ccdbaba9b8288158a5f154c72a3d0779a838
LinaAna <linacarolinadesouza1107@gmail.com>
2026-08-27 18:42:42 -0300

    Configura Jest e cria testes para taxaEntrega

---
+++ delivery.test.js
@@ -0,0 +1,7 @@
+import { taxaEntrega } from "./delivery.js";
+
+describe("taxaEntrega", () => {
+  test("2 km", () => expect(taxaEntrega(2)).toBe(0));
+  test("5 km", () => expect(taxaEntrega(5)).toBe(6));
+  test("10 km", () => expect(taxaEntrega(10)).toBe(9));
+});
